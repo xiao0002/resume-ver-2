7b71fa484a296ec0f3412808eb2a26686bddf1b6
xiao0002 <qinghao00@gmail.com>
2022-04-02 20:31:03 +0800

    2 files modified, 2 added, 1 deleted

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import './styles.scss';
-var work_exp_template;
-function showTemplate(template, data) {
-  var html = template(data);
-  $('#work-exp-content').html(html);
-}
-
-$(document).ready(function () {
-  var source = $('#work-exp-template').html();
-  work_exp_template = Handlebars.compile(source);
-
-  showTemplate(work_exp_template, jobs);
-});
---
+++ src/js/templateengine.js
@@ -0,0 +1,11 @@
+var work_exp_template;
+function showTemplate(pointer, template, data) {
+  var html = template(data);
+  $(pointer).html(html);
+}
+
+$(document).ready(function () {
+  var source = $('#work-exp-template').html();
+  work_exp_template = Handlebars.compile(source);
+  showTemplate('#work-exp-content', work_exp_template, workexp);
+});

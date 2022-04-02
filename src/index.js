d44b9cd42a4924140eeeefdc1bcc2d2f6759fe80
xiao0002 <qinghao00@gmail.com>
2022-04-02 18:16:27 +0800

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.scss';
+var Work_exp_template;
+function showTemplate(template, data) {
+  var html = template(data);
+  $('#content').html(html);
+}
+
+$(document).ready(function () {
+  var source = $('#Work-exp-template').html();
+  Work_exp_template = Handlebars.compile(source);
+});
